07fc0503c583b9c826c5c535e713a788761afba9
NASIR <eocean@NASIRs-MacBook-Pro.local>
2025-11-14 12:54:42 +0500

    Initial

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "HelloSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "HelloSDK",
+            targets: ["HelloSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "HelloSDK",
+            url: "https://github.com/yourorg/HelloSDK-iOS/releases/download/1.0.0/HelloSDK.zip",
+            checksum: "e4466f33ccc332b3dd63c1a94654ee275eeec74b1035914460d7d0b313c17071"
+        )
+    ]
+)
